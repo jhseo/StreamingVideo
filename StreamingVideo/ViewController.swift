fede92323db53f2e31a223cd80eb8291c867fd76
Junghun Seo <sjh7893@gmail.com>
2020-09-26 17:26:38 +0900

    Initial Commit

---
+++ StreamingVideo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StreamingVideo
+//
+//  Created by JunghunSeo on 2020/09/26.
+//  Copyright © 2020 JunghunSeo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
